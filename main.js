fdb0188ae62593263ff264511763b31a68bb824c
Wendel <wendellduos@hotmail.com>
2024-02-21 02:40:57 -0300

    added encryption|decrpyption logic

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
 const input = document.getElementById("user-input");
-const infoWrp = document.getElementById("info-wrp");
-const outputWrp = document.getElementById("output-wrp");
-const outputWrpScreen = document.getElementById("output-wrp-screen");
 const btnWrp = document.getElementById("btn-wrp");
-
-input.addEventListener("input", () => {
-  infoWrp.style.opacity = "0.6";
-  if (input.value === "") {
-    infoWrp.style.opacity = "1";
-  }
-});
-
-updtOutputWrpBottom();
-insertLargeScreenContent();
+const outputWrp = document.getElementById("output-wrp");
+const outputReplaceable = document.getElementById("output-replaceable");
+const output = document.getElementById("output");
+const copyBtn = document.getElementById("copy-btn");
 
 // encryption logic on click event
-const encryptBtn = document.getElementById("encrypt-btn");
-encryptBtn.addEventListener("click", () => {
-  updtOutputWrpBottom();
+document.getElementById("encrypt-btn").addEventListener("click", () => {
+  if (input.value != "") {
+    insertResultingContent(
+      input.value
+        .replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat")
+    );
+  }
 });
 
 // decryption logic on click event
-const decryptBtn = document.getElementById("decrypt-btn");
-decryptBtn.addEventListener("click", () => {
-  updtOutputWrpBottom();
+document.getElementById("decrypt-btn").addEventListener("click", () => {
+  if (input.value != "") {
+    insertResultingContent(
+      input.value
+        .replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u")
+    );
+  }
+});
+
+function insertResultingContent(convertedTxt) {
+  outputReplaceable.innerHTML = "";
+  output.innerHTML = convertedTxt;
+  copyBtn.style.display = "block";
+}
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(output.textContent);
+
+  // change colors for copy button, end then back to the original color
+  const btnColors = {
+    text: window.getComputedStyle(copyBtn).color,
+    border: window.getComputedStyle(copyBtn).border,
+  };
+
+  copyBtn.innerText = "Copiado!";
+  copyBtn.style.color = "green";
+  copyBtn.style.border = "1px solid green";
+
+  setTimeout(() => {
+    copyBtn.innerText = "Copiar";
+    copyBtn.style.color = btnColors.text;
+    copyBtn.style.border = btnColors.border;
+  }, 1500);
 });
 
 // change outputWrp position based on sceen width
-function updtOutputWrpBottom() {
-  if (!screen.availWidth > 1440) {
-    btnWrp.style.bottom = `${outputWrp.clientHeight + 60}px`;
-  } else {
-    btnWrp.style.top = `${screen.availHeight - btnWrp.clientHeight - 50}px`;
-  }
-}
+// function updtOutputWrpBottom() {
+//   if (!screen.availWidth > 1440) {
+//     btnWrp.style.bottom = `${outputWrp.clientHeight + 60}px`;
+//   } else {
+//     btnWrp.style.top = `${screen.availHeight - btnWrp.clientHeight - 50}px`;
+//   }
+// }
 
-window.addEventListener("resize", () => {
-  insertLargeScreenContent();
-});
+// window.addEventListener("resize", () => {
+//   insertLargeScreenContent();
+// });
 
-function insertLargeScreenContent() {
-  if (screen.availWidth >= 1440) {
-    const image = `<img src="./assets/guy-illustration.svg" alt="Ilustração de um menino com segurando uma lupa" />`;
-    const currentContent = outputWrpScreen.innerHTML;
+// function insertLargeScreenContent() {
+//   if (screen.availWidth >= 1440) {
+//     const image = `<img src="./assets/guy-illustration.svg" alt="Ilustração de um menino com segurando uma lupa" />`;
+//     const currentContent = outputWrpScreen.innerHTML;
 
-    outputWrpScreen.innerHTML = `${image}${currentContent}`;
-  }
-}
-
-// encryption logic
+//     outputWrpScreen.innerHTML = `${image}${currentContent}`;
+//   }
+// }
